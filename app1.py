87d74d7c8d30a6aac5119d2cff9566d49ce65f99
mysgrown <gauravtsm@gmail.com>
2024-04-04 13:31:24 +0530

    ok

---
+++ app1.py
@@ -0,0 +1,296 @@
+import requests
+import json
+import streamlit as st
+from pathlib import Path
+import numpy as np
+import altair as alt
+import pandas as pd
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
+from datetime import time, datetime
+import time
+
+# Just add it after st.sidebar:
+a = st.sidebar.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+st.sidebar.write('You selected:', a)
+
+st.sidebar.write ('What would you like to order?')
+
+icecream = st.sidebar.checkbox('Ice cream')
+coffee = st.sidebar.checkbox('Coffee')
+cola = st.sidebar.checkbox('Cola')
+
+if icecream:
+     st.sidebar.write("Great! Here's some more 🍦")
+
+if coffee: 
+     st.sidebar.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.sidebar.write("Here you go 🥤")
+
+
+st.title('How to layout your Streamlit app')
+
+with st.expander('About this app'):
+  st.write('This app shows the various ways on how you can layout your Streamlit app.')
+  st.image('https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png', width=250)
+
+st.sidebar.header('Input')
+user_name = st.sidebar.text_input('What is your name?')
+user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
+user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
+
+st.header('Output')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+  if user_name != '':
+    st.write(f'👋 Hello {user_name}!')
+  else:
+    st.write('👈  Please enter your **name**!')
+
+with col2:
+  if user_emoji != '':
+    st.write(f'{user_emoji} is your favorite **emoji**!')
+  else:
+    st.write('👈 Please choose an **emoji**!')
+
+with col3:
+  if user_food != '':
+    st.write(f'🍴 **{user_food}** is your favorite **food**!')
+  else:
+    st.write('👈 Please choose your favorite **food**!')
+
+st.title('st.progress')
+
+with st.expander('About this app'):
+    #  st.write('You can now display the progress of your calculations in a Streamlit app with the `st.progress` command.')
+    pass
+
+# my_bar = st.progress(0)
+
+# for percent_complete in range(100):
+#      time.sleep(0.05)
+#      my_bar.progress(percent_complete + 1)
+
+# st.balloons()
+
+
+st.title('st.file_uploader')
+
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Descriptive Statistics')
+  st.write(df.describe())
+else:
+  st.info('☝️ Upload a CSV file')
+
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
+
+st.write('Your favorite color is ', option)
+ 
+st.write("""
+# Video Editing & Manipulation Tool using AI
+Vool.AI 
+""")
+st.file_uploader('Upload video file to edit')
+
+
+st.header('st.button')
+
+if st.button('Say hello'):
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
+
+
+st.header('st.write')
+
+# Example 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Example 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
+
+# st.header('`streamlit_pandas_profiling`')
+
+# df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+# pr = df.profile_report()
+# st_profile_report(pr)
+
+st.header('st.latex')
+
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
+
+st.title('st.form')
+
+# Full example of using the with notation
+st.header('1. Example of using `with` notation')
+st.subheader('Coffee machine')
+
+with st.form('my_form'):
+    st.subheader('**Order your coffee**')
+
+    # Input widgets
+    coffee_bean_val = st.selectbox('Coffee bean', ['Arabica', 'Robusta'])
+    coffee_roast_val = st.selectbox('Coffee roast', ['Light', 'Medium', 'Dark'])
+    brewing_val = st.selectbox('Brewing method', ['Aeropress', 'Drip', 'French press', 'Moka pot', 'Siphon'])
+    serving_type_val = st.selectbox('Serving format', ['Hot', 'Iced', 'Frappe'])
+    milk_val = st.select_slider('Milk intensity', ['None', 'Low', 'Medium', 'High'])
+    owncup_val = st.checkbox('Bring own cup')
+
+    # Every form must have a submit button
+    submitted = st.form_submit_button('Submit')
+
+if submitted:
+    st.markdown(f'''
+        ☕ You have ordered:
+        - Coffee bean: `{coffee_bean_val}`
+        - Coffee roast: `{coffee_roast_val}`
+        - Brewing: `{brewing_val}`
+        - Serving type: `{serving_type_val}`
+        - Milk: `{milk_val}`
+        - Bring own cup: `{owncup_val}`
+        ''')
+else:
+    st.write('☝️ Place your order!')
+
+
+# Short example of using an object notation
+st.header('2. Example of object notation')
+
+form = st.form('my_form_2')
+selected_val = form.slider('Select a value')
+form.form_submit_button('Submit')
+
+st.write('Selected value: ', selected_val)
+
+number = st.sidebar.slider('Select a number:', 0, 10, 5)
+st.write('Selected number from slider widget is:', number)
+
+st.title('st.experimental_get_query_params')
+
+with st.expander('About this app'):
+  st.write("`st.experimental_get_query_params` allows the retrieval of query parameters directly from the URL of the user's browser.")
+
+st.title('st.session_state')
+
+def lbs_to_kg():
+  st.session_state.kg = st.session_state.lbs/2.2046
+def kg_to_lbs():
+  st.session_state.lbs = st.session_state.kg*2.2046
+
+st.header('Input')
+col1, spacer, col2 = st.columns([2,1,2])
+with col1:
+  pounds = st.number_input("Pounds:", key = "lbs", on_change = lbs_to_kg)
+with col2:
+  kilogram = st.number_input("Kilograms:", key = "kg", on_change = kg_to_lbs)
+
+st.header('Output')
+st.write("st.session_state object:", st.session_state)
+
+st.title('🏀 Bored API app')
+
+st.sidebar.header('Input')
+selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
+
+suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+json_data = requests.get(suggested_activity_url)
+suggested_activity = json_data.json()
+
+c1, c2 = st.columns(2)
+with c1:
+  with st.expander('About this app'):
+    st.write('Are you bored? The **Bored API app** provides suggestions on activities that you can do when you are bored. This app is powered by the Bored API.')
+with c2:
+  with st.expander('JSON data'):
+    st.write(suggested_activity)
+
+st.header('Suggested activity')
+st.info(suggested_activity['activity'])
+
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.metric(label='Number of Participants', value=suggested_activity['participants'], delta='')
+with col2:
+  st.metric(label='Type of Activity', value=suggested_activity['type'].capitalize(), delta='')
+with col3:
+  st.metric(label='Price', value=suggested_activity['price'], delta='')
+
+
+  st.title('🖼️ yt-img-app')
+st.header('YouTube Thumbnail Image Extractor App')
+
+with st.expander('About this app'):
+  st.write('This app retrieves the thumbnail image from a YouTube video.')
+
+# Image settings
+st.sidebar.header('Settings')
+img_dict = {'Max': 'maxresdefault', 'High': 'hqdefault', 'Medium': 'mqdefault', 'Standard': 'sddefault'}
+selected_img_quality = st.sidebar.selectbox('Select image quality', ['Max', 'High', 'Medium', 'Standard'])
+img_quality = img_dict[selected_img_quality]
+
+yt_url = st.text_input('Paste YouTube URL', 'https://youtu.be/JwSS70SZdyM')
+
+def get_ytid(input_url):
+  if 'youtu.be' in input_url:
+    ytid = input_url.split('/')[-1]
+  if 'youtube.com' in input_url:
+    ytid = input_url.split('=')[-1]
+  return ytid
+
+# Display YouTube thumbnail image
+if yt_url != '':
+  ytid = get_ytid(yt_url) # yt or yt_url
+
+  yt_img = f'http://img.youtube.com/vi/{ytid}/{img_quality}.jpg'
+  st.image(yt_img)
+  st.write('YouTube video thumbnail image URL: ', yt_img)
+else:
+  st.write('☝️ Enter URL to continue ...')
